547b8901f4afa953faeefb51fc7be52da7602e9f
ThyWoof <pauloesquilo@gmail.com>
2021-04-20 21:35:18 -0700

    more tweaks

--- SolastaUnofficialTranslations/Main.cs
+++ SolastaUnofficialTranslations/Main.cs
@@ -18,26 +18,32 @@ namespace SolastaUnofficialTranslations
         public string code, text, file;
     }
 
-    static class Languages
+    static class Language
     {
+        const String IN = "Translation-";
+        const String OUT = "Export-";
+        const String EXT = ".txt";
+
+        private static readonly LanguageSourceData languageSourceData = LocalizationManager.Sources[0];
+
         private static readonly List<LanguageEntry> languages = new List<LanguageEntry>();
 
         private static void Error(string msg) => Main.Error(msg);
 
-        public static void DetermineLanguages()
+        public static void LoadLanguages()
         {
             CultureInfo[] cultureInfos = CultureInfo.GetCultures(CultureTypes.AllCultures);
             DirectoryInfo directoryInfo = new DirectoryInfo($@"{UnityModManager.modsPath}/{typeof(Main).Namespace}");
-            FileInfo[] files = directoryInfo.GetFiles("Translation-*.txt");
+            FileInfo[] files = directoryInfo.GetFiles($"{IN}*{EXT}");
 
             foreach(var file in files)
             {
-                var code = file.Name.Substring(12, file.Name.Length - 4 - 12);
+                var code = file.Name.Substring(IN.Length, file.Name.Length - IN.Length - EXT.Length);
                 var cultureInfo = cultureInfos.First<CultureInfo>(o => o.Name == code);
                 if (cultureInfo == null)
-                    Error("unrecognized language: " + file.Name);
+                    Error($"unrecognized language {code} on {file.Name}");
                 else if (LocalizationManager.HasLanguage(cultureInfo.DisplayName))
-                    Error("language already in game: " + file.Name);
+                    Error($"language {code} from {file.Name} already in game");
                 else
                     languages.Add(new LanguageEntry()
                     {
@@ -52,10 +58,8 @@ namespace SolastaUnofficialTranslations
 
         public static int Count() => languages.Count;
 
-        public static void LoadTranslations()
+        public static void LoadTerms()
         {
-            var languageSourceData = LocalizationManager.Sources[0];
-
             // load new language terms
             foreach (var language in languages)
             {
@@ -75,15 +79,14 @@ namespace SolastaUnofficialTranslations
                     }
                 }
             }
+        }
 
-            //// load languages titles and descriptions across all languages including unofficial ones
-            //foreach (var language in languages)
-            //    foreach (var languageData in languageSourceData.mLanguages)
-            //    {
-            //        var i = languageSourceData.GetLanguageIndex(languageData.Name);
-            //        languageSourceData.AddTerm("Setting/&TextLanguage" + language.code + "Title").Languages[i] = language.text;
-            //        languageSourceData.AddTerm("Setting/&TextLanguage" + language.code + "Description").Languages[i] = language.text;
-            //    }
+        internal static void ExportTerms(int languageIndex, String code)
+        {
+            var outputFilename = $@"{UnityModManager.modsPath}/{typeof(Main).Namespace}/{OUT}{code}{EXT}";
+            using (var sw = new StreamWriter(outputFilename))
+                foreach (var term in languageSourceData.mTerms)
+                    sw.WriteLine($"{term.Term} {term.Languages[languageIndex]?.Replace("\n", @"\n")}");
         }
     }
 
@@ -103,8 +106,8 @@ namespace SolastaUnofficialTranslations
                 Logger = modEntry.Logger;
                 modEntry.OnGUI = OnGUI;
 
-                Languages.DetermineLanguages();
-                Languages.LoadTranslations();
+                Language.LoadLanguages();
+                Language.LoadTerms();
 
                 var harmony = new Harmony(modEntry.Info.Id);
                 harmony.PatchAll(Assembly.GetExecutingAssembly());
@@ -122,29 +125,12 @@ namespace SolastaUnofficialTranslations
         {
             var languageSourceData = LocalizationManager.Sources[0];
             foreach(var languageData in languageSourceData.mLanguages)
-                if (GUILayout.Button($"Export {languageData.Name} Language"))
+                if (GUILayout.Button($"Export {languageData.Name}"))
                 {
-                    ExportTerms(languageSourceData.GetLanguageIndex(languageData.Name), languageData.Code);
+                    Language.ExportTerms(languageSourceData.GetLanguageIndex(languageData.Name), languageData.Code);
                 }
-        }
-
-        internal static void ExportTerms(int languageIndex, String code)
-        {
-            var languageSourceData = LocalizationManager.Sources[0];
-            var outputFilename = $@"{UnityModManager.modsPath}/{typeof(Main).Namespace}/Export-{code}.txt";
-            using (var sw = new StreamWriter(outputFilename))
-            {
-                foreach (var term in languageSourceData.mTerms)
-                {
-                    sw.WriteLine("{0} {1}",
-                        term.Term,
-                         term.Languages[languageIndex] != null ?
-                            term.Languages[languageIndex].Replace("\n", @"\n").Replace("\t", "") : "");
-                }
-            }
         }
     }
-
 
     [HarmonyPatch(typeof(GameManager), "BindServiceSettings")]
     internal static class GameManager_BindServiceSettings_Patch
@@ -152,7 +138,7 @@ namespace SolastaUnofficialTranslations
         public static void Prefix(Dictionary<string, string> ___languageByCode)
         {
             if (___languageByCode != null)
-                foreach (LanguageEntry language in Languages.GetAll())
+                foreach (LanguageEntry language in Language.GetAll())
                     if (!___languageByCode.ContainsKey(language.code))
                         ___languageByCode.Add(language.code, language.text);
         }
@@ -169,11 +155,11 @@ namespace SolastaUnofficialTranslations
             if (___settingTypeDropListAttribute?.Name == "TextLanguage")
             {
                 int top = ___settingTypeDropListAttribute.Items.Count<String>();
-                String[] items = new String[top + Languages.Count()];
+                String[] items = new String[top + Language.Count()];
                 Array.Copy(___settingTypeDropListAttribute.Items, items, top);
                 ___settingTypeDropListAttribute.Items = items;
 
-                foreach (LanguageEntry language in Languages.GetAll())
+                foreach (LanguageEntry language in Language.GetAll())
                 {
                     items[top++] = language.code;
                     ___dropList.options.Add(new GuiDropdown.OptionDataAdvanced 
